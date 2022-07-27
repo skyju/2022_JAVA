90639afa06d775221c6d6e67297f402cc1ece7cf
skyju <kacamimi@naver.com>
2022-07-27 17:59:23 +0900

    commit

--- 2022_JAVA/src/day0727/Solution_SWEA_1220Magneic_D3_000ms.java
+++ 2022_JAVA/src/day0727/Solution_SWEA_1220Magneic_D3_000ms.java
@@ -10,14 +10,45 @@ public class Solution_SWEA_1220Magneic_D3_000ms {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		for (int tc = 1; tc <= 10; tc++) {
-			// 푸 ->N
-			// 붉 ->S
+			// 1 N 위 
+			// 2 S 아래
 			br.readLine();
+			
+			int[][] arr = new int[100][100];
+			
+			// 입력
 			for (int i = 0; i < 100; i++) {
-				for (int j = 0; j < 100; j++) {
-
+				String line = br.readLine();
+				for (int j = 0, index = 0; j < 100; j++, index += 2) {
+					arr[i][j] = line.charAt(index);
 				}
 			}
+			
+			int previous;
+			int ans = 0;
+			// 행단위 탐색은 안해도되고 하 방향으로만 보면됨
+			for (int j = 0; j < 100; j++) {
+				previous = 2;
+				int tmp = 0;
+				for (int i = 0; i < 100; i++) {
+					if (arr[i][j] == '0') continue;
+					else {
+						if (previous != arr[i][j]) { //교착상태
+							if (tmp != 1) {
+								++ans;
+								if (ans == 1) {
+									ans += 1;
+								}
+							}
+							tmp++;
+						}
+						previous = arr[i][j];
+					}
+				}
+				System.out.println(j + "열의 교착상태 누적 " + ans);
+			}
+			
+			System.out.println(ans);
 		}
 	}
 }
